40e5004aebf70fbb4b1bda769e3b969cd231b69a
Robin <BeBinaries>
2017-11-23 21:16:18 +0530

    added saving function and put output in form of latex

--- app/src/main/java/com/example/robin/tabbedact/MathTab2.java
+++ app/src/main/java/com/example/robin/tabbedact/MathTab2.java
@@ -2,8 +2,12 @@ package com.example.robin.tabbedact;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
@@ -19,6 +23,11 @@ import android.widget.Toast;
 import com.myscript.atk.math.widget.MathWidgetApi;
 import com.myscript.certificate.MyCertificate;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.util.Date;
+
 
 /**
  * Created by robin on 7/10/17.
@@ -30,6 +39,7 @@ public class MathTab2 extends Fragment implements
 
     private MathWidgetApi widget;
     private static final String TAG = "MathDemo";
+    private int count =0;
     private Bitmap bmp;
 
     @Override
@@ -38,6 +48,7 @@ public class MathTab2 extends Fragment implements
         View rootView = inflater.inflate(R.layout.math_window2, container, false);
         Button b1 = (Button)rootView.findViewById(R.id.clearButton);
         Button b2 = (Button)rootView.findViewById(R.id.sendButton);
+        Button b3 =(Button)rootView.findViewById(R.id.saveButton);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,6 +59,18 @@ public class MathTab2 extends Fragment implements
             @Override
             public void onClick(View view) {
                 onSendButtonClick(view);
+            }
+        });
+        //Asking for permission for sdfVersion > 23
+        if (Build.VERSION.SDK_INT >= 23 && ActivityCompat.checkSelfPermission(getActivity(),android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bmp = widget.getResultAsImage();
+
+                saveToDownloads(bmp);
             }
         });
         return rootView;
@@ -133,8 +156,28 @@ public class MathTab2 extends Fragment implements
     private void onSendButtonClick(View v)
     {
         Context mContext = getActivity() ;
-        //new MainActivity().send(widget.getResultAsLaTeX(),mContext);
-        bmp = widget.getResultAsImage();
-        new MainActivity().BitMapToString(bmp,mContext);
+        new MainActivity().send(widget.getResultAsLaTeX(),mContext);
+        /*bmp = widget.getResultAsImage();
+        new MainActivity().BitMapToString(bmp,mContext);*/
+
+    }
+
+
+    private void saveToDownloads(Bitmap bitmap) {
+        FileOutputStream fos = null;
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        String fileName = "Mat"+currentDateTimeString;
+        count++;
+        try {
+            if (ActivityCompat.checkSelfPermission(getActivity(),android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
+                fos = new FileOutputStream(file);
+                // Use the compress method on the BitMap object to write image to the OutputStream
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                Toast.makeText(getContext(),"Saved to downloads folder", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
