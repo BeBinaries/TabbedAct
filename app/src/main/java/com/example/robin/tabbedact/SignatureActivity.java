afc2a5b2a6732ab67e7169b08e88db95b2075e69
Shashank1089 <32129395+Shashank1089@users.noreply.github.com>
2017-11-29 16:13:22 +0530

    sending signature - update

--- app/src/main/java/com/example/robin/tabbedact/SignatureActivity.java
+++ app/src/main/java/com/example/robin/tabbedact/SignatureActivity.java
@@ -1,19 +1,24 @@
 package com.example.robin.tabbedact;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class SignatureActivity extends Activity {
 
     private CanvasView customCanvas;
-
+    LinearLayout x ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_alert_sign);
 
         customCanvas = (CanvasView) findViewById(R.id.signature_canvas);
+
+
+
     }
 
     public void clearCanvas(View v) {
@@ -22,6 +27,9 @@ public class SignatureActivity extends Activity {
     }
     public void sendBit(View v)
     {
-        customCanvas.sendSignature();
+        LinearLayout x = (LinearLayout) findViewById(R.id.linlay);
+        x.setDrawingCacheEnabled(true);
+        x.buildDrawingCache();
+        customCanvas.sendSignature(x);
     }
 }
